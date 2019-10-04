0e2f8601abe60bc6b68599194af14c626915eb1f
Tapas <tapasnayak74@gmail.com>
2019-10-05 01:50:11 +0530

    enviroment variable added

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const app = express();
+var port = process.env.port || 3000;
 
 app.get('/',(req,resp,next)=>{
     resp.send('<h1>Testing app in App engine</h1>')
 });
 
-app.listen(1234,()=>{
-    console.log("App starts listening on port 1234")
+app.listen(port,()=>{
+    console.log(`App starts listening on port ${ port }`)
 })
